603d4e0f4adefb7d1905a756b5bed9efb0b3fcb8
alex.logorz <alex.logorz@gmail.com>
2021-05-04 17:21:50 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -3,33 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const nodemailer = require('nodemailer');
+const path = require("path");
+const sendRouter = require('./routes/send')
 
+
+app.use(cors());
 app.use(express.json());
+// app.use(express.static("../client/build"));
+app.use('/', sendRouter);
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    }
-});
-
-app.post('/send', (req, res) => {
-    let mailOptions = {
-        from: 'baiterxrs@gmail.com',
-        to: 'baiterxrs@gmail.com',
-        subject: 'Testing',
-        text: req.body.message
-    }
-   
-    transporter.sendMail(mailOptions, function(err, data) {
-        if(err)
-            res.send('Message failed to send.');
-        else
-            res.status(200).send('Message sent.');
-    });
-    
-});
 
 app.listen(port, () => console.log(`listening on port ${port}`));
